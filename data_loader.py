ce0a6452557b3d111d7f57957d9d3f99244cb45f
Fiona Xu <50728665+YingXu001@users.noreply.github.com>
2023-11-13 10:39:44 -0600

    update difference source of data loader

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,25 @@
+import json
 from torch_geometric.datasets import Planetoid, Reddit
-from torch_geometric.transforms import NormalizeFeatures
 
-def load_data(dataset_name):
+def load_graph_data(dataset_name):
     if dataset_name == 'Cora':
-        dataset = Planetoid(root='/tmp/Cora', name='Cora', transform=NormalizeFeatures())
+        dataset = Planetoid(root='/tmp/Cora', name='Cora')
     elif dataset_name == 'Reddit':
         dataset = Reddit(root='/tmp/Reddit')
-    elif dataset_name == 'Hellaswag':
-        pass
-    # Add other datasets as needed
-
+    else:
+        raise ValueError(f"Unsupported graph dataset: {dataset_name}")
     data = dataset[0]
     return data, dataset
+
+def load_text_data(file_path):
+    with open(file_path, 'r') as file:
+        data_list = json.load(file)
+    return data_list
+
+def load_data(dataset_type, dataset_name=None, file_path=None):
+    if dataset_type == 'graph':
+        return load_graph_data(dataset_name)
+    elif dataset_type == 'text':
+        return load_text_data(file_path)
+    else:
+        raise ValueError(f"Unsupported dataset type: {dataset_type}")
--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def set_seed(seed):
 
 def parse_args():
     parser = argparse.ArgumentParser(description="GCN for different datasets")
-    parser.add_argument('--dataset', type=str, default='Cora', help='Dataset name (Cora, Reddit, etc.)')
+    parser.add_argument('--dataset_type', type=str, required=True, choices=['graph', 'text'], help='Type of the dataset (graph or text)')
+    parser.add_argument('--dataset_name', type=str, help='Name of the graph dataset (e.g., Cora, Reddit)')
+    parser.add_argument('--file_path', type=str, help='File path for the text dataset')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--patience', type=int, default=40, help='Patience for early stopping')
     parser.add_argument('--epochs', type=int, default=500, help='Number of epochs to train')
@@ -29,17 +31,25 @@ def main():
     # Create logs directory if it doesn't exist
     os.makedirs('logs', exist_ok=True)
 
-    log_filename = f'logs/{args.dataset}_training_result.log'
-    data, dataset = load_data(args.dataset)
+    # Load data based on the dataset type
+    if args.dataset_type == 'graph':
+        data, dataset = load_data(dataset_type='graph', dataset_name=args.dataset_name)
+    elif args.dataset_type == 'text':
+        data = load_data(dataset_type='text', file_path=args.file_path)
+        dataset = None  # For text datasets, there might not be a 'dataset' object
+    else:
+        raise ValueError("Invalid dataset type specified")
+
+    log_filename = f'logs/{args.dataset_name}_training_result.log'
     train_losses, val_losses, val_accuracies = train_model(data, dataset, args.lr, args.patience, args.epochs, log_filename)
     
-    plot_losses(train_losses, val_losses, args.dataset)
-    plot_accuracies(val_accuracies, args.dataset)
+    plot_losses(train_losses, val_losses, args.dataset_name)
+    plot_accuracies(val_accuracies, args.dataset_name)
 
     test_loss, test_accuracy = test_model(data, dataset)
 
     # Save results to log
-    with open(f'{args.dataset}_testing_result.log', 'w') as log_file:
+    with open(f'{args.dataset_name}_testing_result.log', 'w') as log_file:
         log_file.write(f'Test Loss: {test_loss}, Test Accuracy: {test_accuracy}\n')
 
 if __name__ == "__main__":
